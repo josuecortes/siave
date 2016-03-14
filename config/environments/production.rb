b02aff9e410dcd61ea08ac1109c8162074eff73e
= <=>
2016-03-14 13:34:02 -0300

    layout production.rb 4

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,7 +23,7 @@ Rails.application.configure do
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   #config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
-  config.serve_static_files = false
+  #config.serve_static_files = false
   config.assets.debug = true
   config.assets.digest = true
 
